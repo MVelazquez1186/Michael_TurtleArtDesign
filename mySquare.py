61eb7be42793cf1937ef26dfd000e5e8c7411298
MVelazquez <33318792+MVelazquez1186@users.noreply.github.com>
2017-11-03 08:20:14 -0400

    combined randint and This file

--- mySquare.py
+++ mySquare.py
@@ -20,6 +20,26 @@ def move( t, x, y):
     t.goto(x,y)
     t.pendown()
 
+from random import randint
+from mySquare import *
+import turtle
+turtle.colormode(255)
+
+bob = turtle.Turtle()
+bob.speed(0)
+
+for times in range(1000):
+    x = randint(-600,600)
+    y = randint(-400,400)
+    s = randint(4,10)
+    red = randint(0,255)
+    green = randint(0,255)
+    blue = randint(0,255)
+    bob.color(red,green,blue)
+    move(bob,x,y)
+    polygon(bob,20,s)
+
+
  
 
 
